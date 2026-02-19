50772e7b47ed30c6a762e557470ef460b2cd69d7
Enrique Izel <eleaoizel@gmail.com>
2026-02-18 21:04:18 -0400

    fix: update the file name and interface name

--- domain/src/main/java/com/moviefinder/domain/repository/MovieRepository.kt
+++ domain/src/main/java/com/moviefinder/domain/repository/MovieRepository.kt
@@ -2,7 +2,7 @@ package com.moviefinder.domain.repository
 
 import com.moviefinder.domain.model.Movie
 
-interface IMovieRepository {
+interface MovieRepository {
     suspend fun getMovies(): Result<List<Movie>>
 
     suspend fun getMovieDetails(id: Int): Result<Movie>
